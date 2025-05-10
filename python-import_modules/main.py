d360124b88ed8a0c58fb79e64931061c33ee3226
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-10 21:09:58 +0000

    Task 00

--- python-import_modules/main.py
+++ python-import_modules/main.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-# Import the add function from add_0.py
 from add_0 import add
+
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    # Print the result using string format
+
     print("{} + {} = {}".format(a, b, add(a, b)))
