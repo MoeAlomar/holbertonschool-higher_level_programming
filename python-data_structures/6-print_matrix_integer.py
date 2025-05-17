3592c4cad5dac90de332682649a2d14dd0633657
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-17 22:16:27 +0000

    task 07?

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[[]]) <= 0:
+    return None
+    for i in range(len(matrix)):
+
+            for j in range(len(matrix[i])):
+                if j < len(martix[i]) - 1:
+                print("{:d}".format(martix[i][j],end = " "))
+                else:
+                    print("{:d}".format(martix[i][j],end = ""))
+        print()
