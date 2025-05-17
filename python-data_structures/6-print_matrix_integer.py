cc9badad5537897ab4507cbb8ec8036c29988299
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-17 22:21:19 +0000

    error fixes

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix[[]]) <= 0:
-    return None
-    for i in range(len(matrix)):
+    if not matrix or not matrix[0]:
+        return None
 
-            for j in range(len(matrix[i])):
-                if j < len(martix[i]) - 1:
-                print("{:d}".format(martix[i][j],end = " "))
-                else:
-                    print("{:d}".format(martix[i][j],end = ""))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j < len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
         print()
