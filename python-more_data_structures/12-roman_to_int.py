7e76e48c09b8145c0c229c6dd44ad1f28a230b7a
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-18 16:05:58 +0000

    if statement corrected

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    if (not roman_string or type(roman_string) != str):
+    if not isinstance(roman_string, str) or not roman_string:
         return total
     for i in range(len(roman_string) - 1, -1, -1):
         num_roman = roman[roman_string[i]]
