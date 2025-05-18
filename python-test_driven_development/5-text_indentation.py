7609dd8f9323aa850ece017765d8741f3c677624
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-18 20:59:43 +0000

    Task 5 fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,21 +3,19 @@
 
 
 def text_indentation(text):
-    """String validation"""
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+   """String validation"""
+   if not isinstance(text, str):
+       raise TypeError("text must be a string")
 
-    
-    sep = [".", "?", ":"]
-    idx_prev = 0
+   sep = [".", "?", ":"]
+   idx_prev = 0
 
-    
-    for i in range(len(text)):
-        if i == len(text) - 1:
-            print(text[idx_prev:i + 1], end="")
-        elif text[i] in sep:
-            print(text[idx_prev:i + 1] + '\n')
-            idx_prev = i + 1
-            
-            while text[idx_prev] == " ":
-                idx_prev += 1
+   for i in range(len(text)):
+       if i == len(text) - 1:
+           print(text[idx_prev:i + 1], end="")
+       elif text[i] in sep:
+           print(text[idx_prev:i + 1] + '\n')
+           idx_prev = i + 1
+
+           while text[idx_prev] == " ":
+               idx_prev += 1
