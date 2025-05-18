a455bb204013689441844df8b3f3549db7dc3c56
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-18 08:53:55 +0000

    typo

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     c0 = tuple_a[0] + tuple_b[0]
-    c1 = tuple_1[1] + tuple_b[1]
+    c1 = tuple_a[1] + tuple_b[1]
 
     tuple_c = (c0, c1)
     return tuple_c
