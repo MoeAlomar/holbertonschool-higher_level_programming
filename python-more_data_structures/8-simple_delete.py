492c543f73ec528b95282726ad8201bf5dc0ac34
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-18 11:40:58 +0000

    task 8 fix

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    return del a_dictionary[key] if key in a_dictionary else a_dictionary
+ del a_dictionary[key] if key in a_dictionary else a_dictionary
+return a_dictionary
