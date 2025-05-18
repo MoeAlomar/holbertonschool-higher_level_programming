01b3047b6fe8eb3293969b76784476a9c6c706ac
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-18 20:36:12 +0000

    whitespaces

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,11 +2,11 @@
 """ adds two integer """
 
 def add_integer(a, b=98):
-    """ 
+    """
     Args:
     a: integer or float variable
     b: integer or float variable
-    
+
     """
 
     if not isinstance(a, (int, float)):
