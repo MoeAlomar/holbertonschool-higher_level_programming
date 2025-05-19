0d644f3a0511de1aaa836456f97ad55578945e60
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-19 08:50:26 +0000

    task 1 Fix

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""This module is a subclass of the super class Square
-it defines a square based on the super class"""
-class Square(Square):
+"""This module defines a Square class with a private size attribute.
+
+The size of the square is stored privately to enforce encapsulation.
+"""
+class Square():
     """Here in this class we will define __init__ method which will assign the __size value"""
     def __init__(self,size):
         """This method will assign the private  attribute size value
-        with the recieved size without any checking on value or type validation!!"""
+        with the recieved size without any checking on value or type validation!!
+        Args:
+        size: the size of the square should be an integer"""
         self.__size = size
