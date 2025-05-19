d7e6eee34b25e0a7f03a1010b618128a080d8246
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-19 07:58:52 +0000

    Task 1 try

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining a Square class that prints a square with #"""
+class Square:
+    def __init__(self, size):
+        self.size = size
+        for i in range(size):
+            for i int range(size):
+                print("#", end="")
+            print()
