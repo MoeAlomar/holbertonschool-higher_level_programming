b35153316243a7fd07140b2fd767e74f580664bb
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-19 09:18:29 +0000

    2nd Task try 100000

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""This module is based on 1-square.py which will create a square
-
-the size of square is taken from size and stored privatly also it will be value and type checked
-"""
+"""This module creates a class named Square"""
 
 
 class Square:
-    """ Represents square with private size attribute."""
+    """A class named Square
+
+    Attributes:
+    attr1 (size): size of square
+    """
     def __init__(self, size=0):
-        """we will initialize size in this method with optional size = 0
-
-        Args:
-            size: this will be the size of the square. Must be an integer >= 0.
-
-        Raises:
-            TypeError: if size type is not integer.
-            ValueError: if size value is less than 0
         """
-        if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+        Args:
+        size (int): size for __size attribute of class instance
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
