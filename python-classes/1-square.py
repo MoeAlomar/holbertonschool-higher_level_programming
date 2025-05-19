9586f6a4b3b226aea6eb776387fe77195c4f6ea6
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-19 08:57:44 +0000

    pycoding style?

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,9 +4,11 @@
 The size of the square is stored privately to enforce encapsulation.
 """
 
+
 class Square:
     """Here in this class we will define __init__ method which will assign the __size value"""
-    def __init__(self,size):
+
+    def __init__(self, size):
         """initializes the square with a given size.
 
         Args:
