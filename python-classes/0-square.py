f3415b4736ab1f0d7162e08e9ae8c1c05ed892cb
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-19 08:02:37 +0000

    making the Square class only defines a Square and nothing else

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Defining a Square class that prints a square with #"""
+"""Defining a Square class only."""
 class Square:
-    def __init__(self, size):
-        self.size = size
-        for i in range(size):
-            for i int range(size):
-                print("#", end="")
-            print()
+    pass
