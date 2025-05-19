ae5f82282a50324c7b9b36250d76f85a5bfea380
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-19 08:35:43 +0000

    Task 0 fix 6th try.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""This module creates an empty class named Square"""
+"""This module creates an empty class named Square
+that will be a super class to continues sub classes."""
 
 
 class Square:
-    """An empty class"""
+    """This is an empty class used as a starting point
+    for defining square-related properties and methods."""
     pass
