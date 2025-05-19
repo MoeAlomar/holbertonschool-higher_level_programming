65c0cb7c6fb19e512bf68e9847f25034c068c433
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-19 08:56:02 +0000

    Task 1 pycoding Style Validation check

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,11 +3,13 @@
 
 The size of the square is stored privately to enforce encapsulation.
 """
-class Square():
+
+class Square:
     """Here in this class we will define __init__ method which will assign the __size value"""
     def __init__(self,size):
-        """This method will assign the private  attribute size value
-        with the recieved size without any checking on value or type validation!!
+        """initializes the square with a given size.
+
         Args:
-        size: the size of the square should be an integer"""
+            size: the size of the square should be an integer (no type/value validation).
+        """
         self.__size = size
