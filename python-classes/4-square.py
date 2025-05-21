eb04335e1c2c8c871abdc5d14776f056e276a02a
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-21 08:46:59 +0000

    Task 4  init commit.

---
+++ python-classes/4-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""This module creates a class named Square
+
+It makes a square with a specified size.
+"""
+
+
+class Square:
+    """Represents a square with a validated size.
+
+    The size must be an integer >= 0. It is stored as a private attribute
+    initialized by default to 0.
+    """
+    def __init__(self, size=0):
+        """Initializes an instance of square
+
+        Args:
+            size (int): size for __size attribute of class instance
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Defined area method in class square
+
+        Returns:
+            (int): area of the square be returning self.__size * self.__size.
+        """
+        return self.__size ** 2
+    
+    def size(self):
+        """Getter method of size instance
+
+        Returns:
+            (int): size of square.
+        """
+        return size
+    
+    def size(self, value):
+        """Setter method to instance size.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size value is < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
