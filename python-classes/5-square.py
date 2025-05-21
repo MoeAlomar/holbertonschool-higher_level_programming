901515cbcd377abdb63859aa2791ec0cfcbc6855
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-21 09:29:31 +0000

    task 5 fix, i used print without specifying end="", and used __size instead of self.__size which is wrong

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -71,7 +71,7 @@ class Square:
         """
         if self.__size == 0:
             print()
-        for i in range(__size):
-            for j in range(__size):
-                print("#")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
             print()
