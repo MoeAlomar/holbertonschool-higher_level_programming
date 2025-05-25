644c6eb866e6ffff37cb7103b7970c053246e9b2
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-25 07:17:55 +0000

    Task 0 Making rectangle class that defines rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Empty Rectangle module that defines Rectangle class."""
+
+
+class Rectangle:
+"""Empty Rectangle class used as a starting point to the next rectangle classes in the rest tasks."""
+    pass
