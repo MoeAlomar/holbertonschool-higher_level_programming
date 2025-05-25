8e12d685b938813acfa221f48debe5136dab30ac
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-25 09:59:57 +0000

    Task 9 init commit.

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -155,3 +155,9 @@ class Rectangle:
             return rect_1
         return rect_2
     def square(cls, size=0):
+        """Return a new Rectangle with width and height equal to size.
+
+        Args:
+            size (int): The width and height of the new Rectangle.
+        """
+        return (cls(size, size))
