4acefc4d5d6eb47ce40cfa77f7cffd83ce5b291c
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-25 08:39:22 +0000

    if statement fixed

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -118,6 +118,6 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 rect.append("#")
-            if i != self.__height:
+            if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
