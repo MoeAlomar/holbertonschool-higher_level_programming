2311e27c971f54fc06560377f329ed530e715851
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-25 07:20:06 +0000

    module not documented correctly fixed.

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Empty Rectangle module that defines Rectangle class."""
+"""Empty Rectangle module that defines Rectangle class
+and from this i will start my tasks."""
 
 
 class Rectangle:
-"""Empty Rectangle class used as a starting point to the next rectangle classes in the rest tasks."""
+    """Empty Rectangle class used as a starting point to
+    the next rectangle classes in the rest tasks."""
     pass
