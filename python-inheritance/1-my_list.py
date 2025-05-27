012454989927dde84a632794e3a054c87e4f47bb
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-27 08:31:50 +0000

    pep8

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,6 +8,8 @@ class MyList(list):
     """here we are recieving list and sort it using sort function in python
     """
     my_list = list
+
+    
     def print_sorted(self):
         """
         print_sorted is a method that will print the list of the class
