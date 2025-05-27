a2f675afb52aea532d298a6d2232901c7fb01717
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-27 07:02:59 +0000

    task 0 init commit

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+"""here we define lookup class, a class that looks up for an object's method."""
+def lookup(obj):
+    """this class returns the list of all methods and attributes of an object.
+    """
+    return dir(obj)
