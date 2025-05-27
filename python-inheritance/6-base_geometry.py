a845922dabebb7bed1726fb951494f266069e65b
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-27 08:58:58 +0000

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""BaseGeometry class that has unimplemented area function"""
+
+
+class BaseGeometry():
+    """A class with area() function that raises an exception
+    because it not implemented yet"""
+    def area(self):
+        """Empty area method.
+        
+        Raises:
+            Exception: because area function is not implemented yet.
+        """
+        raise Exception("area() is not implemented")
