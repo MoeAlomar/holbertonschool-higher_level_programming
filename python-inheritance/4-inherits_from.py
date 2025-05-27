f54707d269acdde2971f50a6dc88bcfeb2d18235
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-27 08:50:36 +0000

    task 4 init commit

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A class that checks if an object is of the same class"""
+
+
+def inherits_from(obj, a_class):
+    """this function checks if the
+    provided function is of an inherited class provided but 
+    not from the same class
+
+    Args:
+        obj: is an object from any type.
+        a_class: a class that returns true of the obj is instance of it.
+
+    Returns:
+        True: if the class has this object.
+        False: otherwisse.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
