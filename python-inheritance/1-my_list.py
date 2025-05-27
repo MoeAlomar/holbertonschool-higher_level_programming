f1299005bcc69896d5e66bc8486901274584c676
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-27 07:32:11 +0000

    indentation error fixed.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,9 +8,9 @@ class MyList(list):
     """here we are recieving list and sort it using sort function in python"""
     my_list = list
     def print_sorted(self):
-    """
-    print_sorted is a method that will print the list of the class
-    """
-    sorted_my_list = my_list
-    sorted_my_list.sort()
-    print(self.sorted_my_list)
+        """
+        print_sorted is a method that will print the list of the class
+        """
+        sorted_my_list = my_list
+        sorted_my_list.sort()
+        print(self.sorted_my_list)
