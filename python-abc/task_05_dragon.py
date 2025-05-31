129eae735ab8653406731c38d73213eeb7c62fd2
MoeAlomar <Mohammedahalomar@gmail.com>
2025-05-31 15:33:10 +0000

    PEP8

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-
-
 class SwimMixin:
     """A mixin class that provides swimming behavior.
 
     Methods:
         swim(): Prints a message indicating the creature swims.
     """
+
     def swim(self):
         """Prints a message indicating the creature swims."""
         print("The creature swims!")
 
 
 class FlyMixin:
-    """This class defines a fly method that prints a statement."""
+    """A mixin class that provides flying behavior.
+
+    Methods:
+        fly(): Prints a message indicating the creature flies.
+    """
 
     def fly(self):
+        """Prints a message indicating the creature flies."""
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
-    """This class defines a roar method that prints a statement."""
+    """A class representing a dragon that can swim and fly.
+
+    Methods:
+        roar(): Prints a message indicating the dragon roars.
+    """
 
     def roar(self):
-        print("The dragon Roars!")
+        """Prints a message indicating the dragon roars."""
+        print("The dragon roars!")
