6409dcc3b8ab29a0ba3c11a24bdf1ddd79f887df
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-03 08:25:26 +0000

    PEP8 Validation

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,31 +2,34 @@
 """
 Module: class_to_obj
 
-This module provides functionality to convert class instances to dictionary objects.
-It contains a utility function that extracts the instance attributes of an object
+This module provides functionality to convert class
+instances to dictionary objects.
+It contains a utility function that extracts
+the instance attributes of an object
 and returns them as a dictionary representation.
 
 Functions:
-    class_to_obj(obj): Converts an object's instance attributes to a dictionary.
+    class_to_obj(obj): Converts an object's instance
+    attributes to a dictionary.
 """
 
 
 def class_to_json(obj):
     """
     Convert an object's instance attributes to a dictionary.
-    
+
     This function takes any object and returns a dictionary containing
     all of its instance attributes (stored in the object's __dict__).
     This is commonly used for serialization purposes, such as converting
     objects to JSON format.
-    
+
     Args:
         obj: Any object instance that has a __dict__ attribute.
-    
+
     Returns:
         dict: A dictionary containing the object's instance attributes
               as key-value pairs.
-    
+
     Example:
         >>> class Person:
         ...     def __init__(self, name, age):
