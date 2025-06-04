86d93751c49c46db4e9879ac2a6732c30056e965
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-04 11:02:01 +0000

    Task 00 init commit.

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+import json
 
+
+def serialize_and_save_to_file(data, filename):
+    """a function to serialize data to a json file"""
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """this loads the file content and deserialaizes it
+    after doing it returns it"""
+    with open(filename, "r") as f:
+        return json.load(f)
