469a0bee4e80149111c2ab9253920b474e7577ee
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-14 21:01:11 +0000

    task 03 fix

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,12 +34,9 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
         else:
             self.send_response(404)
-            self.send_header("Content-type", "application/json")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            response_data = {
-                "error": "Endpoint not found"
-            }
-            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+            self.wfile.write(b"Endpoint not found")
 
 
 Handler = MyHandler
