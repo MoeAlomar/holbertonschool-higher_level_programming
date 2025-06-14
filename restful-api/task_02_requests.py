69ecb4995dd21e361b1b60545b9d588e38f5edb8
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-14 20:46:16 +0000

    task 02 init commit.

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+    print(f"Status code: {r.status_code}")
+    if r.status_code == 200:
+        posts = r.json()
+        for post in posts:
+          print(post[title])
+
+def fetch_and_save_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+    if r.status_code == 200:
+        posts = r.json()
+
+        listed_posts = []
+        for post in posts:
+            listed.posts.append({
+                'id' : post['id'],
+                'title' : post['title']
+                'body' : post['body']
+            })
+
+        with open("posts.csv", "w",encoding="utf-8") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(listed_posts)
