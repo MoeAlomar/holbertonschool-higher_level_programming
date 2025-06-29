8fb47b081fc7e6a0e3a6319a47833760b3548a99
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-29 12:50:33 +0000

    SQL queries TASK 1 Typo fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,4 @@
--- Creates the MySQL server user user_0d_1 with all privileges
+-- script that creates the MySQL server user user_0d_1.
 
--- Create user user_0d_1@localhost with password if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges on the server to user_0d_1@localhost
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply the changes immediately
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
