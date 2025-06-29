7d4a7e8fc1e5f0af86249f5ac24568747d024b48
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-29 11:24:09 +0000

    SQL TASK 10 init commit

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- List all records from the table second_table, ordered by score (top first)
+
+-- Select score and name from second_table and sort by score descending
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
