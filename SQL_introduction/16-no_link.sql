8f53bf8535cebe9f1958fa56d2d034c652ab73c7
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-29 11:48:42 +0000

    SQL TASK 16 init commit

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,7 +1,7 @@
 -- List all records from the table second_table where name is not empty or NULL
 
 -- Select score and name, ignoring records with NULL or empty name, ordered by score descending
-SELECT SCORE, NAME
+SELECT score, name
 FROM second_table
 WHERE NAME IS NOT NULL AND NAME != ''
-ORDER BY SCORE DESC;
+ORDER BY score DESC;
