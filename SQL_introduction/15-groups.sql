03140b3487f87ffb3aea9f581f9a7f45275c1ee9
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-29 11:46:31 +0000

    SQL TASK 15 Capital Letters

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- List the number of records for each score in the table second_table
 
 -- Select the score and count how many times each score appears, sorted by count descending
-SELECT SCORE, COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
