f18b6203272106e5d48ba4c93c8d9ae6acea2423
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-29 12:18:02 +0000

    SQL queries TASK 0 init commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2.
 -- Showing grant permissions for user_0d_1
-SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 -- showing grant permissions of user_0d_2
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
