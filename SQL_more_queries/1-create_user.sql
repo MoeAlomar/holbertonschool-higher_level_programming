432dc6facff83208188d0cdfa7aa178e9512402c
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-29 12:31:23 +0000

    SQL queries TASK 1 Typo fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,9 @@
--- creats a user with all privileges
--- creating user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED
-BY 'user_0d_1_pwd';
+-- Creates a user with all privileges
+-- Creating user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Granting all privilegs
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'lcoalhost' WITH GRANT OPTION;
+-- Granting all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply privilege changes immediately
 FLUSH PRIVILEGES;
