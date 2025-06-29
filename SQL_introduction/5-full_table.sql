76ebda29c13684e85d0449562ac4d2ed99fd50d9
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-29 10:59:55 +0000

    SQL TASK 5 FIX

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0
--- Use INFORMATION_SCHEMA to query table structure without DESCRIBE or EXPLAIN
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Using SHOW CREATE TABLE to retrieve the full DDL
+SHOW CREATE TABLE first_table;
