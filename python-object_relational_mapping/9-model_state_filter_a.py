b68cd3f08c0afa449d9c95ddbc69360cef084294
MoeAlomar <Mohammedahalomar@gmail.com>
2025-06-30 10:20:53 +0000

    Task 9 pycodingstyle

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     states = session.query(State).filter(
         State.name.like('%a%')).order_by(State.id)
     for state in states:
-            print(f"{state.id}: {state.name}")
+        print(f"{state.id}: {state.name}")
 
     session.close()
