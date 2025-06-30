4f54da5ecbb9ceab4a5f6ffb310620a3243ad805
Mohammad Alomar <Mohammedahalomar@gmail.com>
2025-06-30 11:47:13 +0300

    Task 5 Validating and splitting state arguments

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,17 +6,29 @@ import re
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
+
+    if (len(argv) != 5):
+
+        print("You must be use mysql username, mysql password,\
+               database name and state name")
+        exit(1)
+
+
     try:
         db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                              passwd=argv[2],db=argv[3])
     except Exception:
         print("Can't connect to database")
         exit(0)
-    state = argv[4]
-    if re.search('^[a-zA-Z]+$', state) is None:
+
+    state = ' '.join(argv[4].split())
+
+    if (re.search('^[a-zA-Z]+$', state) is None):
         print('Please enter a valid name State')
         exit(1)
+
     cursor = db.cursor()
+
     cursor.execute("SELECT cities.name, FROM cities\
                     INNER JOIN states ON cities.state_id=states.id\
                     WHERE states.name = '{:s}' ORDER BY cities.id ASC;".format(state))
